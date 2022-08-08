87cce16a69a23e68af643bcea1289159c8048236
danielgallo <daniel.gallo@gmail.com>
2022-08-08 18:50:44 +0100

    Enable CORS

--- GivTCP/REST.py
+++ GivTCP/REST.py
@@ -2,11 +2,13 @@
 # version 2021.12.22
 
 from flask import Flask, json, request
+from flask_cors import CORS
 import GivTCP.read as rd       #grab passthrough functions from main read file
 import write as wr      #grab passthrough functions from main write file
 
 #set-up Flask details
 giv_api = Flask(__name__)
+CORS(giv_api)
 
 #Proxy Read Functions
 
